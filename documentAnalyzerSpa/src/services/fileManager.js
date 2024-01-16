1436a78d2023fe2fd0c9ca62d6d08ff6e414b268
Julio Andres Bolaños Pulido <julioandres.bp@outlook.com>
2024-01-16 11:29:01 -0500

    fix endpoint path

--- documentAnalyzerSpa/src/services/fileManager.js
+++ documentAnalyzerSpa/src/services/fileManager.js
@@ -1,7 +1,7 @@
 export async function uploadFile(file){
     const data = new FormData();
     data.append('file', file);
-    const response = await fetch(`${import.meta.env.VITE_FILE_MANAGER_API_HOST}/filemanager/upload`, {
+    const response = await fetch(`${import.meta.env.VITE_FILE_MANAGER_API_HOST}/FileManager`, {
         method: 'POST',
         body: data
     });
